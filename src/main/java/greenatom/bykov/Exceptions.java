822bf2b988b541c912885bd2889ff76dbd4d0cd3
79969 <m.byckoff2015@yandex.ru>
2023-09-19 20:30:31 +0300

    feat: add wrong usage of exceptions example

---
+++ src/main/java/greenatom/bykov/Exceptions.java
@@ -0,0 +1,39 @@
+package greenatom.bykov;
+
+public class Exceptions {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            try {
+                new MyException();//static init ar only throws once
+            } catch (Throwable e) {
+                System.out.println("e = " + e);
+                if (e.getCause() instanceof MyException) {
+                    MyException ex = (MyException) e.getCause();
+                    System.out.println("e instanceof " + MyException.class.getName() + ", s = " + ex.get());
+                }
+            }
+        }
+    }
+}
+
+class MyException extends RuntimeException {
+    private String s;
+
+    static {
+        init();
+    }
+
+    public MyException() {
+        s = "Hello";
+    }
+
+    private static void init() {
+        throw new MyException();
+    }
+
+    public String get() {
+        return s;
+    }
+}
+
+
